f6db7333205c908ede25f7ef7fcb590f9b88cbe7
liyabarsieva-sys <liya.barsieva@gmail.com>
2026-05-05 18:13:03 +0400

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export { PLATFORMS } from './platforms';
+export { TONES, LENGTH_OPTIONS, PILLAR_ANGLES } from './writing';
+export { AWARENESS_STAGES, RUBRICS, CTA_OPTIONS, RUBRIC_SORDELL_MAP } from './strategy';
+export { SORDELL_MATRIX } from './sordell';
+export { CAROUSEL_TEMPLATES, HOOK_TYPES } from './content';
+export { BRAND_ARCHETYPES, PLATFORM_FREQ_HINTS } from './brands';
